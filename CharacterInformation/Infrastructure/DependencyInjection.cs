84fb48dfb43a3caaa01a5e47adf204072a5a4cf3
I-Nhut Nguyen <nguyenvannhut0704@gmail.com>
2023-03-15 17:06:39 +0700

    add DependencyInjection

---
+++ CharacterInformation/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(defaultConnectionString));
+
+            return services;
+        }
+    }
+}
