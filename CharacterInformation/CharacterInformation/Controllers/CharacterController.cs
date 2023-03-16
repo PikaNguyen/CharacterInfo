227d4a0de5efd262136389e6d57b7e846be7c654
I-Nhut Nguyen <nguyenvannhut0704@gmail.com>
2023-03-16 15:36:12 +0700

    create Implement
    create Implement Character

---
+++ CharacterInformation/CharacterInformation/Controllers/CharacterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CharacterInformation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+    }
+}
